b84aa7542fa6d2d84a7a60273dd37400005c5d04
Zeeshan1234 <mayub9799@gmail.com>
2023-12-03 21:37:36 +0500

    day 2 - part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let games = std::fs::read_to_string("./input.txt").expect("Failed to read input file");
     
-    let (red, green, blue) = (12,13,14);
+    // let (red, green, blue) = (12,13,14);
     let mut result = 0;
     for game in games.lines() {
         let mut parts = game.split(":");
-        let id = parts.next().unwrap().split_whitespace().last().unwrap().parse::<u32>().expect("failed to parse game id");
+        let _id = parts.next().unwrap().split_whitespace().last().unwrap().parse::<u32>().expect("failed to parse game id");
 
         let cube_sets = parts.next().unwrap().split(";").map(|cubes| {
             cubes.split(",").map(|cube| {
@@ -16,28 +16,52 @@ fn main() {
             }).collect::<Vec<_>>()
         }).collect::<Vec<_>>();
 
-        let mut possible = true;
+        // let mut possible = true;
+        // for cube_set in cube_sets.iter() {
+        //     let mut red_count = 0;
+        //     let mut green_count = 0;
+        //     let mut blue_count = 0;
+        //     for (count, color) in cube_set.iter() {
+        //         match *color {
+        //             "red" => red_count += count,
+        //             "green" => green_count += count,
+        //             "blue" => blue_count += count,
+        //             _ => panic!("Unknown color {}", color)
+        //         }
+        //     }
+        //     if red_count > red || green_count > green || blue_count > blue {
+        //         println!("Game: {} and cubes sets {:?} not possible", id, cube_sets);
+        //         possible = false;
+        //         break;
+        //     }
+        // }
+        // if possible {
+        //     result += id;
+        // }
+        let (mut maxr,mut maxg,mut maxb) = (0,0,0);
         for cube_set in cube_sets.iter() {
-            let mut red_count = 0;
-            let mut green_count = 0;
-            let mut blue_count = 0;
             for (count, color) in cube_set.iter() {
                 match *color {
-                    "red" => red_count += count,
-                    "green" => green_count += count,
-                    "blue" => blue_count += count,
+                    "red" => {
+                        if count > &maxr {
+                            maxr = *count;
+                        }
+                    },
+                    "green" => {
+                        if count > &maxg {
+                            maxg = *count;
+                        }
+                    },
+                    "blue" => {
+                        if count > &maxb {
+                            maxb = *count;
+                        }
+                    },
                     _ => panic!("Unknown color {}", color)
                 }
             }
-            if red_count > red || green_count > green || blue_count > blue {
-                println!("Game: {} and cubes sets {:?} not possible", id, cube_sets);
-                possible = false;
-                break;
-            }
         }
-        if possible {
-            result += id;
-        }
+        result += maxr * maxg * maxb;
     }
     println!("Result: {}", result);
 }
